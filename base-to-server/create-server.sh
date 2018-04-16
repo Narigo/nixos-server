80b9bfd8ddba21d5e4b56c1da6b64a456b7f9ab8
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-04-16 18:06:42 +0200

    Add sudoers file, remove root pw
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- base-to-server/create-server.sh
+++ base-to-server/create-server.sh
@@ -1,15 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
 
 CHANNEL=18.03
-USERNAME=joern
+USERNAME="$USER"
 SSH_KEY=$(cat "$HOME/.ssh/id_rsa.pub")
 SERVER_IP=2a02:810d:200:1f84::3
 
-cat custom.nix | sed 's/\$USERNAME\$/'"$USERNAME"'/;' | sed 's#\$SSH_KEY\$#'"$SSH_KEY"'#;' > .custom.nix.tmp
-ssh root@$SERVER_IP "cat - > /etc/nixos/custom.nix && nixos-rebuild switch" < .custom.nix.tmp
+# Set up initial root stuff and remove root password again
+cat custom.nix | sed 's/\$USERNAME\$/'"$USERNAME"'/g;' | sed 's#\$SSH_KEY\$#'"$SSH_KEY"'#g;' > .custom.nix.tmp
+scp nextcloud.nix .custom.nix.tmp "root@$SERVER_IP:/etc/nixos/"
+ssh "root@$SERVER_IP" "mv /etc/nixos/.custom.nix.tmp /etc/nixos/custom.nix && nixos-rebuild switch && passwd -d root"
 rm .custom.nix.tmp
 
 # Install more .nix files, this time use the user with their ssh key instead of relying on a password
-ssh $USERNAME@$SERVER_IP "cat - > /etc/nixos/nextcloud.nix && nixos-rebuild switch" < nextcloud.nix
+ssh "$USERNAME@$SERVER_IP" "sudo cat - > /etc/nixos/nextcloud.nix && nixos-rebuild switch" < nextcloud.nix
 
 # TODO: Set password of user
-# TODO: Remove password of root
--- base-to-server/custom.nix
+++ base-to-server/custom.nix
@@ -1,17 +1,23 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ nextcloud.nix ];
+  imports = [ "./nextcloud.nix" ];
   services.sshd.enable = true;
   services.sshd.permitRootLogin = pkgs.lib.mkOverride 40 "no";
 
   systemd.services.sshd.wantedBy = pkgs.lib.mkOverride 40 [ "multi-user.target" ];
 
-  users.users.joern = {
+  security.sudo.enable = true;
+  security.sudo.configFile = ''
+root		ALL = (ALL) ALL
+%wheel		ALL = (ALL) ALL
+'';
+
+  users.users.$USERNAME$ = {
     isNormalUser = true;
     home = "/home/$USERNAME$";
     description = "$USERNAME$";
-    extraGroups = [ "wheel" ];
+    extraGroups = [ "sudoers" "wheel" ];
     openssh.authorizedKeys.keys = [ "$SSH_KEY$" ];
   };
 
